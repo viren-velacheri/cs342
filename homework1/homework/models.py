18ad461d2b3d66cb28272685b6daf79ec11f63c5
viren-velacheri <viren.velacheri@utexas.edu>
2021-09-19 18:40:03 -0500

    Homework 1 stuff

---
+++ homework1/homework/models.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn.functional as F
+
+
+class ClassificationLoss(torch.nn.Module):
+    def forward(self, input, target):
+        """
+        Your code here
+
+        Compute mean(-log(softmax(input)_label))
+
+        @input:  torch.Tensor((B,C))
+        @target: torch.Tensor((B,), dtype=torch.int64)
+
+        @return:  torch.Tensor((,))
+
+        Hint: Don't be too fancy, this is a one-liner
+        """
+        return torch.nn.CrossEntropyLoss(input, target)
+
+
+class LinearClassifier(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+        """
+        Your code here
+        """
+
+    def forward(self, x):
+        """
+        Your code here
+
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        """
+        return self.linear(x.view(x.shape[0], -1))
+
+
+class MLPClassifier(torch.nn.Module):
+    def __init__(self, input_dim, hidden_size):
+        super().__init__()
+        self.linear = torch.nn.Sequential(torch.nn.Linear(input_dim, hidden_size), torch.nn.ReLU(), torch.nn.Linear(hidden_size, output_dim))
+    def forward(self, x):
+        """
+        Your code here
+
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        """
+        return self.linear(x.view(x.shape[0], -1))
+
+
+model_factory = {
+    'linear': LinearClassifier,
+    'mlp': MLPClassifier,
+}
+
+
+def save_model(model):
+    from torch import save
+    from os import path
+    for n, m in model_factory.items():
+        if isinstance(model, m):
+            return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), '%s.th' % n))
+    raise ValueError("model type '%s' not supported!" % str(type(model)))
+
+
+def load_model(model):
+    from torch import load
+    from os import path
+    r = model_factory[model]()
+    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % model), map_location='cpu'))
+    return r
