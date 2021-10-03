5e59a4c99da5a5b371165160a1593f9bc402fd2e
viren-velacheri <viren.velacheri@utexas.edu>
2021-10-03 07:12:42 +0000

    Training is all done

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -9,19 +9,34 @@ class CNNClassifier(torch.nn.Module):
         super().__init__()
         input_channels = 3
         num_classes = 6
-        self.conv = torch.nn.Conv2d(input_channels, 16, 7, 2, 3)
-        self.cls = torch.nn.Linear(16, num_classes)
-
+        self.layers = torch.nn.Sequential(
+            self.__block(input_channels, 32, (7, 1, 3)),
+            self.__block(32, 64),
+            self.__block(64, 128),
+            torch.nn.AvgPool2d(2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(2048, num_classes)
+        )
+        # self.conv = torch.nn.Conv2d(input_channels, 16, 7, 2, 3)
+        # self.cls = torch.nn.Linear(16, num_classes)
+    def __block(self, in_dim, out_dim, extra=(3, 1, 1), pool=(2, 2)):
+      return torch.nn.Sequential(
+          torch.nn.Conv2d(in_dim, out_dim, *extra),
+          torch.nn.BatchNorm2d(out_dim),
+          torch.nn.SiLU(),
+          torch.nn.MaxPool2d(*pool),
+      )
     def forward(self, x):
         """
         Your code here
         @x: torch.Tensor((B,3,64,64))
         @return: torch.Tensor((B,6))
         """
-        x = self.conv(x)
-        # Global Pooling
-        x = x.mean(dim=(2,3))
-        return self.cls(x)
+        # x = self.conv(x)
+        # # Global Pooling
+        # x = x.mean(dim=(2,3))
+        # return self.cls(x)
+        return self.layers(x)
 
 
 def save_model(model):
